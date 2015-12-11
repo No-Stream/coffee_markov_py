8d76fbba7a86de68cd5a23bd382b91c67e49527d
Jan <a70mik@gmail.com>
2015-12-11 12:31:55 -0800

    recursive goodness in place; takes lists
    recursion code is in place but needs cleanup; main now takes lists of
    urls and iterates through them

--- requests_scrape_sprudge.py
+++ requests_scrape_sprudge.py
@@ -6,8 +6,17 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def main(url):
-    return parse_html_and_write(url)
+
+class Tests(unittest.TestCase):
+    """unittests; TODO"""
+    def test_return_html(self):
+        """tests that you get html; TODO"""
+        self.assertEqual(1, 1)
+
+def main(url_list):
+    """Let's get shit started"""
+    for url in url_list:
+        return parse_html_and_write(url)
 
 def return_html(url = "http://www.google.com"):
     """print html of a given URL"""
@@ -25,27 +34,90 @@ def parse_html_and_write(url = "http://www.google.com"):
     write_page(soup, symbol_free_url)
 
 
-def write_page(soup, symbol_free_url):
+def write_page(soup, symbol_free_url, rec_depth = 0):
     """write page to text file"""
+    write_logfile(soup, True)
     with open(
         symbol_free_url + " " + datetime.datetime.now().strftime(
             "%Y-%m-%d_%H-%M-%S") + '.txt', 'w') as f:
-        #f.write(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-        #f.write("\n" + str(soup.title) + "\n")
         if len(soup.find_all("p"))>0:
             for paragraph in soup.find_all("p"):
                 para_text = paragraph.getText()
                 if len(para_text.split())>5: #filter by >5 words
                     f.write(para_text)
-        if len(soup.find_all("a"))>0:
-            for link in soup.find_all("a"):
-                f.write('a clause fired')
-                try: 
-                    f.write("\n" + "link: " + link.get('href'))
-                except TypeError:
-                    pass
+        def log_links_on_page(soup, default = True):
+            if not default:
+                if len(soup.find_all("a"))>0:
+                    for link in soup.find_all("a", href=True):
+                        try:
+                            f.write("\n" + "link: " + link.get('href'))
+                        except TypeError:
+                            pass
+        def recur(soup, default = True):
+            if default and rec_depth < 2 and len(soup.find_all("a"))>0:
+                for link in soup.find_all("a", href=True):
+                    try:
+                        if re.match(r"(https?)://[^\s/$.?#].[^\s]*", link.get('href')):
+                            print(link.get('href'))
+                            #parse_html_and_write(link.get('href'))
+                    except TypeError:
+                        pass
+        log_links_on_page(soup, False)
+        recur(soup, True)
+        rec_depth += 1
         f.close()
 
+def write_logfile(soup, default = True):
+    if default:
+        with open(
+            "SCRAPE_LOG" + " " + datetime.datetime.now().strftime(
+                "%Y-%m-%d") + '.txt', 'a') as f:
+            f.write(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+            f.write("\n" + str(soup.title) + "\n" + "\n")
+            f.close()
 
-main('http://www.gutenberg.org/files/216/216-h/216-h.htm')
-#print(soup_of_page("http://www.crummy.com/software/BeautifulSoup/bs4/doc/"))
+
+
+"""full coffee sites for
+TODO: recursive scraping of full sites by regex of *URL*"""
+sites = [
+    "http://49thcoffee.com/",
+    "http://barnine.us/",
+    "https://bluebottlecoffee.com/",
+    "http://ceremonycoffee.com/",
+    "https://counterculturecoffee.com/",
+    "http://www.dogwoodcoffee.com/",
+    "http://www.equatorcoffees.com/",
+    "http://www.fiveelephant.com/",
+    "http://www.intelligentsiacoffee.com/",
+    "http://www.joenewyork.com/",
+    "http://kaldiscoffee.com/",
+    "http://www.lacolombe.com/",
+    "http://madcapcoffee.com/",
+    "http://mrespresso.com/",
+    "http://theroasters.com.au/",
+    "http://www.olympiacoffee.com/",
+    "http://www.onyxcoffeelab.com/",
+    "http://www.pcpfx.com/",
+    "https://www.philsebastian.com/",
+    "http://populace.coffee/",
+    "http://www.ptscoffee.com/",
+    "http://www.reanimatorcoffee.com/",
+    "http://revelatorcoffee.com/",
+    "https://www.sharecoffeeroasters.com/",
+    "http://spyhousecoffee.com/",
+    "http://www.stumptowncoffee.com/",
+    "http://www.tobysestate.com.au/",
+    "http://www.tonyscoffee.com/",
+    "http://www.vervecoffeeroasters.com/",
+    "http://wateravenuecoffee.com/",
+]
+
+"""products pages for coffee roasters"""
+coffee_pages = [
+    "http://49thcoffee.com/collections/shop",
+    "http://barnine.us/collections/all",
+]
+
+#main('http://www.gutenberg.org/files/216/216-h/216-h.htm')
+main(["http://www.crummy.com/software/BeautifulSoup/bs4/doc/"])
