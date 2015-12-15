4bfb9b632e653c0981ac45c5d5586d8e124ede77
Jan <a70mik@gmail.com>
2015-12-14 20:16:00 -0800

    concat source text files; experimenting with Markov options
    extra files created; they will be cleaned up later once the Markov
    generation is sorted out

---
+++ markov/markov_from_text.py
@@ -0,0 +1,7 @@
+"""markov magic goes here"""
+
+import MarkovChain
+
+MarkovChain.generateDatabase("This is a string of Text. It won't generate an interesting database though.")
+
+MarkovChain.generateString()
